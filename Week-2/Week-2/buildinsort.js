11e212a8da536aa6e4165eaaa98429610e05f39d
vinayydv3695 <vinayydv343@gmail.com>
2025-06-17 20:50:19 +0530

    Add Binary Search function for sorted arrays

--- Week-2/Week-2/buildinsort.js
+++
@@ -1,3 +0,0 @@
-let numbers = [50, 20, 40, 10, 30];
-numbers.sort((a, b) => a - b);
-console.log(numbers);
--- Week-2/Week-2/linearsearch.js
+++
@@ -1,8 +0,0 @@
-function linearSearch(arr, target) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === target) {
-      return `Target found at index ${i}`;
-    }
-  }
-  return "Target not found";
-}
