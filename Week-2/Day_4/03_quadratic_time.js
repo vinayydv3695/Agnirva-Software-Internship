2f2248a791ad38c64b75372435c218083bf69d9a
vinayydv3695 <vinayydv343@gmail.com>
2025-06-20 20:14:47 +0530

    Add O(n^2) quadratic time example with nested loops

---
+++ Week-2/Day_4/03_quadratic_time.js
@@ -0,0 +1,9 @@
+// O(n^2) - Quadratic Time Example
+
+let numbers = [1, 2, 3, 4];
+
+for (let i = 0; i < numbers.length; i++) {
+  for (let j = 0; j < numbers.length; j++) {
+    console.log(`i=${i}, j=${j}`);
+  }
+}
