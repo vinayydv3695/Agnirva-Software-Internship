b6e542ca85621bc599c30c14605951d15d5c98e6
vinayydv3695 <vinayydv343@gmail.com>
2025-07-11 10:07:47 +0530

    Add Express server with GET, POST, and DELETE routes

--- Week_3/Day_5/blog-app/server.js
+++ Week_3/Day_5/blog-app/server.js
@@ -29,3 +29,7 @@ const PORT = 5000;
 app.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`),
 );
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Blog API!");
+});
