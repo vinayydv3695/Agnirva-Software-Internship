52f8f5e818486ce2968d11df5933be4b4ee99161
vinayydv3695 <vinayydv343@gmail.com>
2025-07-11 10:16:44 +0530

    Initial commit: setup base project structure

---
+++ Week_4/git-advanced-practice/script.js
@@ -0,0 +1,6 @@
+// Change background color when button is clicked
+document
+  .getElementById("changeColorBtn")
+  .addEventListener("click", function () {
+    document.body.style.backgroundColor = "lightblue";
+  });
